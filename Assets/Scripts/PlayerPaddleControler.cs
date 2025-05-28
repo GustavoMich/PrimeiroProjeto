ec597af7318bcc519716005f5c495773f4f2f13b
GustavoMich <gustavomichalski5@gmail.com>
2025-05-27 21:15:17 -0300

    Primeiro commit

---
+++ Assets/Scripts/PlayerPaddleControler.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPaddleControler : MonoBehaviour
+{
+    public bool isPlayer = true;
+    public SpriteRenderer spriteRenderer;
+
+
+    public float speed = 5f;
+
+    void Start()
+    {
+        if (isPlayer)
+            spriteRenderer.color = SaveController.Instance.colorPlayer;
+        else
+            spriteRenderer.color = SaveController.Instance.colorEnemy;
+    }
+
+
+
+
+    void Update()
+    {
+        float moveInput = Input.GetAxis("Vertical");
+
+        Vector3 newPosition = transform.position + Vector3.up * moveInput * speed * Time.deltaTime;
+
+        newPosition.y = Mathf.Clamp(newPosition.y, -4.5f, 4.5f);
+
+        transform.position = newPosition;
+    }
+}
